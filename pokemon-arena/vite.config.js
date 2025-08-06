14babdaa31d7466fc88fbbeb44c45a3668e55e0e
AustinAdams11 <austinadams011@gmail.com>
2025-08-06 18:03:43 -0400

    shop updates

--- pokemon-arena/vite.config.js
+++ pokemon-arena/vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      // any request starting with /api/nhl will be forwarded...
+      "/api/poke": {
+        target: "https://api.pokemontcg.io/v2/cards?q=set.name:Fossil nationalPokedexNumbers:[1 TO 151]",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api\/poke/, ""),
+      },
+    },
+  },
+});

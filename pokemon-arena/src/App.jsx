aa8b2ddc91bed4214b800c6dbea81df5e619ea5b
yepitsaname <fernando.g.berry@gmail.com>
2025-08-06 15:12:08 -0400

    adds context to application

--- pokemon-arena/src/App.jsx
+++ pokemon-arena/src/App.jsx
@@ -4,18 +4,18 @@ import Home from './Home';
 import Shop from './Shop';
 import OpenPack from './OpenPack';
 import Battle from './Battle';
+import AppContext from './AppContext';
 
 export default function App() {
+  const [funds, setFunds] = useState(1000)
   return (
-    <>
-
+    <AppContext.Provider value={{credits: funds, setCredits: setFunds}}>
       <Routes>
         <Route path='/' element={<Home/>}/>
         <Route path='/shop' element={<Shop/>}/>
         <Route path='/open-packs' element={<OpenPack/>}/>
         <Route path='/battle' element={<Battle/>}/>
       </Routes>
-
-    </>
+    </AppContext.Provider>
   )
 }

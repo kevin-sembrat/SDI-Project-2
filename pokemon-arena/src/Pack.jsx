0f396514a38db87f5a6dd1d5cdef47377f82e916
yepitsaname <fernando.g.berry@gmail.com>
2025-08-07 15:02:59 -0400

    fixes a merge issue

--- pokemon-arena/src/Pack.jsx
+++ pokemon-arena/src/Pack.jsx
@@ -3,10 +3,7 @@ import { getPackImage } from "./utils";
 export default function Pack({pack, count, clickHandler=()=>{}}){
   return (
     <div className='pack' onClick={()=>{clickHandler()}}>
-<<<<<<< HEAD
-=======
         <h3>{pack.name}</h3>
->>>>>>> shop-page
       <img alt='pack-image' src={getPackImage(pack)}/>
       <p>{count}</p>
     </div>

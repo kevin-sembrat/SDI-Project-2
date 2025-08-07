f05b43c852afac4ab0c1be2fb9f6cfa31b10180f
AustinAdams11 <austinadams011@gmail.com>
2025-08-07 15:59:46 -0400

    functional shop

--- pokemon-arena/src/Shop.jsx
+++ pokemon-arena/src/Shop.jsx
@@ -1,7 +1,8 @@
 import { useContext } from 'react';
 import { useState, useEffect } from 'react';
 import './Shop.css'
-import  Pack  from './Pack'
+import  Pack  from './Pack' 
+import { Link } from 'react-router'
 // import AppContext from './AppContext';
 
 
@@ -31,24 +32,41 @@ function Shop() {
     },
   ];
 
-  function addToCart(pack){
-    if(funds>=pack.price)
-      setCart((prevPacks) => [...prevPacks, pack]);
+ function addToCart(pack) {
+  if (funds >= pack.price) {
+    setCart((prevPacks) => [...prevPacks, pack]);
+    setFunds((prevFunds) => prevFunds - pack.price);
+  } else {
+    alert("Not enough funds!");
   }
-  function removeFromCart(){}
+}
+  const removeFromCart = (packId) => {
+  const packIndex = cart.findIndex(pack => pack.id === packId);
+    let updatedCart = [...cart];
+      if (packIndex > -1) {
+    updatedCart.splice(packIndex, 1);
+    setCart(updatedCart);
+    setFunds((prevFunds) => prevFunds + cart[packIndex].price);
+  }
+};
+  
+
 
 
   return (
   <div className="shop-container">
     <h1 className="shop-title">Pokémon Shop</h1>
     <p className="shop-funds">Money: ₱{funds}</p>
+  <div className="Checkout">
+      <Link to='/open-packs'>Open Packs</Link>
+    </div>
 
     <div className="pack-list">
       {packs.map((pack) => (
         <Pack
           key={pack.id}
           pack={pack}
-          count={"₱" + pack.price}
+          count={"₽" + pack.price}
           clickHandler={() => addToCart(pack)}
         />
       ))}
@@ -62,7 +80,8 @@ function Shop() {
         <ul>
           {cart.map((item, index) => (
             <li key={index}>
-              {item.name} - ${item.price}
+             {item.name} - ₱{item.price} {" "}
+             <button onClick={() => removeFromCart(item.id)}>Remove</button> 
             </li>
           ))}
         </ul>
@@ -73,72 +92,3 @@ function Shop() {
 }
 
 export default Shop;
-
-//id, count, image
-//set base1 //exporting packData including id/count/image and base1.
-//..spread operator for adding
-//splicing for removing create new array to push packs into so to not mutate
-//splice index with remove cart inde
-
-//fetch pack data
-// map through for data needed
-//
-// {
-//   "data": {
-//     "id": "swsh1",
-//     "name": "Sword & Shield",
-//     "series": "Sword & Shield",
-//     "printedTotal": 202,
-//     "total": 216,
-//     "legalities": {
-//       "unlimited": "Legal",
-//       "standard": "Legal",
-//       "expanded": "Legal"
-//     },
-//     "ptcgoCode": "SSH",
-//     "releaseDate": "2020/02/07",
-//     "updatedAt": "2020/08/14 09:35:00",
-//     "images": {
-//       "symbol": "https://images.pokemontcg.io/swsh1/symbol.png",
-//       "logo": "https://images.pokemontcg.io/swsh1/logo.png"
-//     }
-//   }
-// }
-  // fetch('https://pokeapi.co/api/v2/item?limit=50')
-
-  //pack images
-  //array storing items selected
-    // const {credits, setCredits} = useContext(AppContext)
-//   const [balance, setBalance] = useState(1000);
-//   const [packs, setPacks] = useState([]);
-//   const [puchased, setPurchased] =useState([]);
-//   const [data, setData] = useState(null);
-//   const [error, setError] = useState(null);
-//   const [loading, setLoading] = useState(true);
-
-
-//   useEffect(() => {
-//     const fetchData = async () => {
-//       try {
-//         const res = await fetch('https://api.pokemontcg.io/v2/cards?q=set.name:Fossil nationalPokedexNumbers:[1 TO 151]', {
-//       headers: {
-//         'X-Api-Key': ''
-//       }
-//     })
-//         if (!res.ok) throw new Error(`HTTP ${res.status}`);
-//         const json = await res.json();
-//         setData(json);
-//       } catch (err) {
-//         setError(err.message);
-//       } finally {
-//         setLoading(false);
-//       }
-//     }
-//     // fetchData()
-//   }, []);
-
-//   if (loading) return <p>Loading…</p>;
-//   if (error) return <p>Error: {error}</p>;
-
-// // console.log(data)
-

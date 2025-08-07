e16bb9c9b1e9a08292c0861698216e51fdcf01d3
kevin-sembrat <kevinsembrat2@gmail.com>
2025-08-07 14:42:42 -0400

    Battle page framework

--- pokemon-arena/src/Battle.jsx
+++ pokemon-arena/src/Battle.jsx
@@ -1,3 +1,75 @@
+import { sample_data } from "./sample-data.js";
+import { useState } from "react";
+import "./Battle.css";
+
 export default function Battle() {
-  return <div>Battle</div>;
+  console.log(sample_data);
+  const [playerBench, setPlayerBench] = useState([]);
+  const enemyBench = [];
+
+  return (
+    <>
+      <div class="app">
+        {/* TITLE AND GAME INFO */}
+        <header class="header">
+          <h3>Battle Board</h3>
+          <p>Credits: 1000</p>
+        </header>
+        {/* PLAYER CARDS */}
+        <aside class="sidebar player">
+          <ul>
+            <li>Pokémon A</li>
+            <li>Pokémon B</li>
+            <li>Pokémon C</li>
+            <li>Pokémon D</li>
+            <li>Pokémon E</li>
+            <li>Pokémon F</li>
+          </ul>
+        </aside>
+
+        {/* ENEMY CARDS  */}
+        <aside class="sidebar enemy">
+          <ul>
+            <li>Pokémon A</li>
+            <li>Pokémon B</li>
+            <li>Pokémon C</li>
+            <li>Pokémon D</li>
+            <li>Pokémon E</li>
+            <li>Pokémon F</li>
+          </ul>
+        </aside>
+
+        {/* CENTER BATTLE BOARD SPACE */}
+        <main class="board">
+          <div class="card-box player">Player Card</div>
+          <div class="card-box enemy">Enemy Card</div>
+        </main>
+
+        {/* BETS SECTION */}
+        <section class="bets">
+          <label for="units">Units:</label>
+          <input id="units" type="number" value="10" />
+
+          <div className="prop-bets">
+            <div>
+              <button>Flareon Moneyline (-210)</button>
+            </div>
+            <div>
+              <label for="hp-after">HP After Match (35.5):</label>
+              <button class="btn">OVER</button>
+              <button class="btn">UNDER</button>
+            </div>
+            <div>
+              <label>HP Spread:</label>
+              <button class="btn">+15.5</button>
+              <button class="btn">-15.5</button>
+            </div>
+          </div>
+        </section>
+        <section class="chatCPT-section">
+          <button class="btn primary">BATTLE</button>
+        </section>
+      </div>
+    </>
+  );
 }
---
+++ pokemon-arena/src/sample-data.js
@@ -0,0 +1,623 @@
+export const sample_data = [
+  {
+    name: "Alakazam",
+    hp: "80",
+    images: {
+      small: "https://images.pokemontcg.io/base1/1.png",
+      large: "https://images.pokemontcg.io/base1/1_hires.png",
+    },
+    rarity: "Rare Holo",
+  },
+  {
+    name: "Blastoise",
+    hp: "100",
+    images: {
+      small: "https://images.pokemontcg.io/base1/2.png",
+      large: "https://images.pokemontcg.io/base1/2_hires.png",
+    },
+    rarity: "Rare Holo",
+  },
+  {
+    name: "Chansey",
+    hp: "120",
+    images: {
+      small: "https://images.pokemontcg.io/base1/3.png",
+      large: "https://images.pokemontcg.io/base1/3_hires.png",
+    },
+    rarity: "Rare Holo",
+  },
+  {
+    name: "Charizard",
+    hp: "120",
+    images: {
+      small: "https://images.pokemontcg.io/base1/4.png",
+      large: "https://images.pokemontcg.io/base1/4_hires.png",
+    },
+    rarity: "Rare Holo",
+  },
+  {
+    name: "Clefairy",
+    hp: "40",
+    images: {
+      small: "https://images.pokemontcg.io/base1/5.png",
+      large: "https://images.pokemontcg.io/base1/5_hires.png",
+    },
+    rarity: "Rare Holo",
+  },
+  {
+    name: "Gyarados",
+    hp: "100",
+    images: {
+      small: "https://images.pokemontcg.io/base1/6.png",
+      large: "https://images.pokemontcg.io/base1/6_hires.png",
+    },
+    rarity: "Rare Holo",
+  },
+  {
+    name: "Hitmonchan",
+    hp: "70",
+    images: {
+      small: "https://images.pokemontcg.io/base1/7.png",
+      large: "https://images.pokemontcg.io/base1/7_hires.png",
+    },
+    rarity: "Rare Holo",
+  },
+  {
+    name: "Machamp",
+    hp: "100",
+    images: {
+      small: "https://images.pokemontcg.io/base1/8.png",
+      large: "https://images.pokemontcg.io/base1/8_hires.png",
+    },
+    rarity: "Rare Holo",
+  },
+  {
+    name: "Magneton",
+    hp: "60",
+    images: {
+      small: "https://images.pokemontcg.io/base1/9.png",
+      large: "https://images.pokemontcg.io/base1/9_hires.png",
+    },
+    rarity: "Rare Holo",
+  },
+  {
+    name: "Mewtwo",
+    hp: "60",
+    images: {
+      small: "https://images.pokemontcg.io/base1/10.png",
+      large: "https://images.pokemontcg.io/base1/10_hires.png",
+    },
+    rarity: "Rare Holo",
+  },
+  {
+    name: "Nidoking",
+    hp: "90",
+    images: {
+      small: "https://images.pokemontcg.io/base1/11.png",
+      large: "https://images.pokemontcg.io/base1/11_hires.png",
+    },
+    rarity: "Rare Holo",
+  },
+  {
+    name: "Ninetales",
+    hp: "80",
+    images: {
+      small: "https://images.pokemontcg.io/base1/12.png",
+      large: "https://images.pokemontcg.io/base1/12_hires.png",
+    },
+    rarity: "Rare Holo",
+  },
+  {
+    name: "Poliwrath",
+    hp: "90",
+    images: {
+      small: "https://images.pokemontcg.io/base1/13.png",
+      large: "https://images.pokemontcg.io/base1/13_hires.png",
+    },
+    rarity: "Rare Holo",
+  },
+  {
+    name: "Raichu",
+    hp: "80",
+    images: {
+      small: "https://images.pokemontcg.io/base1/14.png",
+      large: "https://images.pokemontcg.io/base1/14_hires.png",
+    },
+    rarity: "Rare Holo",
+  },
+  {
+    name: "Venusaur",
+    hp: "100",
+    images: {
+      small: "https://images.pokemontcg.io/base1/15.png",
+      large: "https://images.pokemontcg.io/base1/15_hires.png",
+    },
+    rarity: "Rare Holo",
+  },
+  {
+    name: "Zapdos",
+    hp: "90",
+    images: {
+      small: "https://images.pokemontcg.io/base1/16.png",
+      large: "https://images.pokemontcg.io/base1/16_hires.png",
+    },
+    rarity: "Rare Holo",
+  },
+  {
+    name: "Beedrill",
+    hp: "80",
+    images: {
+      small: "https://images.pokemontcg.io/base1/17.png",
+      large: "https://images.pokemontcg.io/base1/17_hires.png",
+    },
+    rarity: "Rare",
+  },
+  {
+    name: "Dragonair",
+    hp: "80",
+    images: {
+      small: "https://images.pokemontcg.io/base1/18.png",
+      large: "https://images.pokemontcg.io/base1/18_hires.png",
+    },
+    rarity: "Rare",
+  },
+  {
+    name: "Dugtrio",
+    hp: "70",
+    images: {
+      small: "https://images.pokemontcg.io/base1/19.png",
+      large: "https://images.pokemontcg.io/base1/19_hires.png",
+    },
+    rarity: "Rare",
+  },
+  {
+    name: "Electabuzz",
+    hp: "70",
+    images: {
+      small: "https://images.pokemontcg.io/base1/20.png",
+      large: "https://images.pokemontcg.io/base1/20_hires.png",
+    },
+    rarity: "Rare",
+  },
+  {
+    name: "Electrode",
+    hp: "80",
+    images: {
+      small: "https://images.pokemontcg.io/base1/21.png",
+      large: "https://images.pokemontcg.io/base1/21_hires.png",
+    },
+    rarity: "Rare",
+  },
+  {
+    name: "Pidgeotto",
+    hp: "60",
+    images: {
+      small: "https://images.pokemontcg.io/base1/22.png",
+      large: "https://images.pokemontcg.io/base1/22_hires.png",
+    },
+    rarity: "Rare",
+  },
+  {
+    name: "Arcanine",
+    hp: "100",
+    images: {
+      small: "https://images.pokemontcg.io/base1/23.png",
+      large: "https://images.pokemontcg.io/base1/23_hires.png",
+    },
+    rarity: "Uncommon",
+  },
+  {
+    name: "Charmeleon",
+    hp: "80",
+    images: {
+      small: "https://images.pokemontcg.io/base1/24.png",
+      large: "https://images.pokemontcg.io/base1/24_hires.png",
+    },
+    rarity: "Uncommon",
+  },
+  {
+    name: "Dewgong",
+    hp: "80",
+    images: {
+      small: "https://images.pokemontcg.io/base1/25.png",
+      large: "https://images.pokemontcg.io/base1/25_hires.png",
+    },
+    rarity: "Uncommon",
+  },
+  {
+    name: "Dratini",
+    hp: "40",
+    images: {
+      small: "https://images.pokemontcg.io/base1/26.png",
+      large: "https://images.pokemontcg.io/base1/26_hires.png",
+    },
+    rarity: "Uncommon",
+  },
+  {
+    name: "Farfetch'd",
+    hp: "50",
+    images: {
+      small: "https://images.pokemontcg.io/base1/27.png",
+      large: "https://images.pokemontcg.io/base1/27_hires.png",
+    },
+    rarity: "Uncommon",
+  },
+  {
+    name: "Growlithe",
+    hp: "60",
+    images: {
+      small: "https://images.pokemontcg.io/base1/28.png",
+      large: "https://images.pokemontcg.io/base1/28_hires.png",
+    },
+    rarity: "Uncommon",
+  },
+  {
+    name: "Haunter",
+    hp: "60",
+    images: {
+      small: "https://images.pokemontcg.io/base1/29.png",
+      large: "https://images.pokemontcg.io/base1/29_hires.png",
+    },
+    rarity: "Uncommon",
+  },
+  {
+    name: "Ivysaur",
+    hp: "60",
+    images: {
+      small: "https://images.pokemontcg.io/base1/30.png",
+      large: "https://images.pokemontcg.io/base1/30_hires.png",
+    },
+    rarity: "Uncommon",
+  },
+  {
+    name: "Jynx",
+    hp: "70",
+    images: {
+      small: "https://images.pokemontcg.io/base1/31.png",
+      large: "https://images.pokemontcg.io/base1/31_hires.png",
+    },
+    rarity: "Uncommon",
+  },
+  {
+    name: "Kadabra",
+    hp: "60",
+    images: {
+      small: "https://images.pokemontcg.io/base1/32.png",
+      large: "https://images.pokemontcg.io/base1/32_hires.png",
+    },
+    rarity: "Uncommon",
+  },
+  {
+    name: "Kakuna",
+    hp: "80",
+    images: {
+      small: "https://images.pokemontcg.io/base1/33.png",
+      large: "https://images.pokemontcg.io/base1/33_hires.png",
+    },
+    rarity: "Uncommon",
+  },
+  {
+    name: "Machoke",
+    hp: "80",
+    images: {
+      small: "https://images.pokemontcg.io/base1/34.png",
+      large: "https://images.pokemontcg.io/base1/34_hires.png",
+    },
+    rarity: "Uncommon",
+  },
+  {
+    name: "Magikarp",
+    hp: "30",
+    images: {
+      small: "https://images.pokemontcg.io/base1/35.png",
+      large: "https://images.pokemontcg.io/base1/35_hires.png",
+    },
+    rarity: "Uncommon",
+  },
+  {
+    name: "Magmar",
+    hp: "50",
+    images: {
+      small: "https://images.pokemontcg.io/base1/36.png",
+      large: "https://images.pokemontcg.io/base1/36_hires.png",
+    },
+    rarity: "Uncommon",
+  },
+  {
+    name: "Nidorino",
+    hp: "60",
+    images: {
+      small: "https://images.pokemontcg.io/base1/37.png",
+      large: "https://images.pokemontcg.io/base1/37_hires.png",
+    },
+    rarity: "Uncommon",
+  },
+  {
+    name: "Poliwhirl",
+    hp: "60",
+    images: {
+      small: "https://images.pokemontcg.io/base1/38.png",
+      large: "https://images.pokemontcg.io/base1/38_hires.png",
+    },
+    rarity: "Uncommon",
+  },
+  {
+    name: "Porygon",
+    hp: "30",
+    images: {
+      small: "https://images.pokemontcg.io/base1/39.png",
+      large: "https://images.pokemontcg.io/base1/39_hires.png",
+    },
+    rarity: "Uncommon",
+  },
+  {
+    name: "Raticate",
+    hp: "60",
+    images: {
+      small: "https://images.pokemontcg.io/base1/40.png",
+      large: "https://images.pokemontcg.io/base1/40_hires.png",
+    },
+    rarity: "Uncommon",
+  },
+  {
+    name: "Seel",
+    hp: "60",
+    images: {
+      small: "https://images.pokemontcg.io/base1/41.png",
+      large: "https://images.pokemontcg.io/base1/41_hires.png",
+    },
+    rarity: "Uncommon",
+  },
+  {
+    name: "Wartortle",
+    hp: "70",
+    images: {
+      small: "https://images.pokemontcg.io/base1/42.png",
+      large: "https://images.pokemontcg.io/base1/42_hires.png",
+    },
+    rarity: "Uncommon",
+  },
+  {
+    name: "Abra",
+    hp: "30",
+    images: {
+      small: "https://images.pokemontcg.io/base1/43.png",
+      large: "https://images.pokemontcg.io/base1/43_hires.png",
+    },
+    rarity: "Common",
+  },
+  {
+    name: "Bulbasaur",
+    hp: "40",
+    images: {
+      small: "https://images.pokemontcg.io/base1/44.png",
+      large: "https://images.pokemontcg.io/base1/44_hires.png",
+    },
+    rarity: "Common",
+  },
+  {
+    name: "Caterpie",
+    hp: "40",
+    images: {
+      small: "https://images.pokemontcg.io/base1/45.png",
+      large: "https://images.pokemontcg.io/base1/45_hires.png",
+    },
+    rarity: "Common",
+  },
+  {
+    name: "Charmander",
+    hp: "50",
+    images: {
+      small: "https://images.pokemontcg.io/base1/46.png",
+      large: "https://images.pokemontcg.io/base1/46_hires.png",
+    },
+    rarity: "Common",
+  },
+  {
+    name: "Diglett",
+    hp: "30",
+    images: {
+      small: "https://images.pokemontcg.io/base1/47.png",
+      large: "https://images.pokemontcg.io/base1/47_hires.png",
+    },
+    rarity: "Common",
+  },
+  {
+    name: "Doduo",
+    hp: "50",
+    images: {
+      small: "https://images.pokemontcg.io/base1/48.png",
+      large: "https://images.pokemontcg.io/base1/48_hires.png",
+    },
+    rarity: "Common",
+  },
+  {
+    name: "Drowzee",
+    hp: "50",
+    images: {
+      small: "https://images.pokemontcg.io/base1/49.png",
+      large: "https://images.pokemontcg.io/base1/49_hires.png",
+    },
+    rarity: "Common",
+  },
+  {
+    name: "Gastly",
+    hp: "30",
+    images: {
+      small: "https://images.pokemontcg.io/base1/50.png",
+      large: "https://images.pokemontcg.io/base1/50_hires.png",
+    },
+    rarity: "Common",
+  },
+  {
+    name: "Koffing",
+    hp: "50",
+    images: {
+      small: "https://images.pokemontcg.io/base1/51.png",
+      large: "https://images.pokemontcg.io/base1/51_hires.png",
+    },
+    rarity: "Common",
+  },
+  {
+    name: "Machop",
+    hp: "50",
+    images: {
+      small: "https://images.pokemontcg.io/base1/52.png",
+      large: "https://images.pokemontcg.io/base1/52_hires.png",
+    },
+    rarity: "Common",
+  },
+  {
+    name: "Magnemite",
+    hp: "40",
+    images: {
+      small: "https://images.pokemontcg.io/base1/53.png",
+      large: "https://images.pokemontcg.io/base1/53_hires.png",
+    },
+    rarity: "Common",
+  },
+  {
+    name: "Metapod",
+    hp: "70",
+    images: {
+      small: "https://images.pokemontcg.io/base1/54.png",
+      large: "https://images.pokemontcg.io/base1/54_hires.png",
+    },
+    rarity: "Common",
+  },
+  {
+    name: "Nidoran ♂",
+    hp: "40",
+    images: {
+      small: "https://images.pokemontcg.io/base1/55.png",
+      large: "https://images.pokemontcg.io/base1/55_hires.png",
+    },
+    rarity: "Common",
+  },
+  {
+    name: "Onix",
+    hp: "90",
+    images: {
+      small: "https://images.pokemontcg.io/base1/56.png",
+      large: "https://images.pokemontcg.io/base1/56_hires.png",
+    },
+    rarity: "Common",
+  },
+  {
+    name: "Pidgey",
+    hp: "40",
+    images: {
+      small: "https://images.pokemontcg.io/base1/57.png",
+      large: "https://images.pokemontcg.io/base1/57_hires.png",
+    },
+    rarity: "Common",
+  },
+  {
+    name: "Pikachu",
+    hp: "40",
+    images: {
+      small: "https://images.pokemontcg.io/base1/58.png",
+      large: "https://images.pokemontcg.io/base1/58_hires.png",
+    },
+    rarity: "Common",
+  },
+  {
+    name: "Poliwag",
+    hp: "40",
+    images: {
+      small: "https://images.pokemontcg.io/base1/59.png",
+      large: "https://images.pokemontcg.io/base1/59_hires.png",
+    },
+    rarity: "Common",
+  },
+  {
+    name: "Ponyta",
+    hp: "40",
+    images: {
+      small: "https://images.pokemontcg.io/base1/60.png",
+      large: "https://images.pokemontcg.io/base1/60_hires.png",
+    },
+    rarity: "Common",
+  },
+  {
+    name: "Rattata",
+    hp: "30",
+    images: {
+      small: "https://images.pokemontcg.io/base1/61.png",
+      large: "https://images.pokemontcg.io/base1/61_hires.png",
+    },
+    rarity: "Common",
+  },
+  {
+    name: "Sandshrew",
+    hp: "40",
+    images: {
+      small: "https://images.pokemontcg.io/base1/62.png",
+      large: "https://images.pokemontcg.io/base1/62_hires.png",
+    },
+    rarity: "Common",
+  },
+  {
+    name: "Squirtle",
+    hp: "40",
+    images: {
+      small: "https://images.pokemontcg.io/base1/63.png",
+      large: "https://images.pokemontcg.io/base1/63_hires.png",
+    },
+    rarity: "Common",
+  },
+  {
+    name: "Starmie",
+    hp: "60",
+    images: {
+      small: "https://images.pokemontcg.io/base1/64.png",
+      large: "https://images.pokemontcg.io/base1/64_hires.png",
+    },
+    rarity: "Common",
+  },
+  {
+    name: "Staryu",
+    hp: "40",
+    images: {
+      small: "https://images.pokemontcg.io/base1/65.png",
+      large: "https://images.pokemontcg.io/base1/65_hires.png",
+    },
+    rarity: "Common",
+  },
+  {
+    name: "Tangela",
+    hp: "50",
+    images: {
+      small: "https://images.pokemontcg.io/base1/66.png",
+      large: "https://images.pokemontcg.io/base1/66_hires.png",
+    },
+    rarity: "Common",
+  },
+  {
+    name: "Voltorb",
+    hp: "40",
+    images: {
+      small: "https://images.pokemontcg.io/base1/67.png",
+      large: "https://images.pokemontcg.io/base1/67_hires.png",
+    },
+    rarity: "Common",
+  },
+  {
+    name: "Vulpix",
+    hp: "50",
+    images: {
+      small: "https://images.pokemontcg.io/base1/68.png",
+      large: "https://images.pokemontcg.io/base1/68_hires.png",
+    },
+    rarity: "Common",
+  },
+  {
+    name: "Weedle",
+    hp: "40",
+    images: {
+      small: "https://images.pokemontcg.io/base1/69.png",
+      large: "https://images.pokemontcg.io/base1/69_hires.png",
+    },
+    rarity: "Common",
+  },
+];

5ea7aed629bacb3862627a4130408b9ac3c6fdca
yepitsaname <fernando.g.berry@gmail.com>
2025-08-11 09:49:36 -0400

    cleans up some spacing and adds a min height

--- pokemon-arena/src/Pack.jsx
+++ pokemon-arena/src/Pack.jsx
@@ -1,10 +1,10 @@
 import { getPackImage } from "./utils";
-import  './Pack.css' 
+import  './Pack.css'
 
 export default function Pack({pack, count, clickHandler=()=>{}}){
   return (
     <div className='pack' onClick={()=>{clickHandler()}}>
-        <h3>{pack.name}</h3>
+      <h3>{pack.name}</h3>
       <img alt='pack-image' src={getPackImage(pack)}/>
       <p>{count}</p>
     </div>
